58f785bcc0a9ec147b44f61063afa9a7957889c3
Cloud Hu <cloudhu9527@gmail.com>
2024-10-30 17:26:08 +0800

    use bevy_new_2d template
    cargo generate thebevyflock/bevy_new_2d

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+// Disable console on Windows for non-dev builds.
+#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
+
+use bevy::prelude::*;
+use skywalker2077::AppPlugin;
+
+fn main() -> AppExit {
+    App::new().add_plugins(AppPlugin).run()
 }
